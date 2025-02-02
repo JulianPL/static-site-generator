2e8adef2ba02697c32ef8aba4d2ba46d55ec08f1
JulianPL <j.pape-lange@gmx.de>
2025-02-02 02:00:35 +0100

    :spakles: Add Leafnodes

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -6,11 +6,22 @@ class HTMLNode:
         self.props = props
 
     def to_html(self):
-        raise NotImplentedError
+        raise NotImplementedError
 
     def props_to_html(self):
         return ' '.join(list(map(lambda key: f"{key}={self.props[key]}", self.props)))
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError("LeafNode without value is not allowed")
+        if not self.tag:
+            return self.value
+        return f"<{self.tag} {self.props_to_html()}>{self.value}</{self.tag}>"
 
--- src/test_htmlcode.py
+++ src/test_htmlcode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html(self):
@@ -10,11 +10,31 @@ class TestHTMLNode(unittest.TestCase):
         props_set = set(HTMLNode(props = {"color": "red", "font": "arial"}).props_to_html().split(' '))
         text_set = set(["color=red", "font=arial"])
         self.assertEqual(props_set, text_set)
+    
+    def test_to_html(self):
+        node = HTMLNode(props = {"color": "red"})
+        with self.assertRaises(NotImplementedError):
+            node.to_html()
 
     def test_repr(self):
         node = HTMLNode("test_tag", "test_value", [HTMLNode("child_tag")], {"color": "red"})
         text = "HTMLNode(test_tag, test_value, [HTMLNode(child_tag, None, None, None)], {'color': 'red'})"
         self.assertEqual(str(node), text)
+
+class TestLeafNode(unittest.TestCase):
+    def test_to_html(self):
+        node = LeafNode("test_tag", None, {"color": "red"})
+        with self.assertRaises(ValueError):
+            node.to_html()
+        node = LeafNode(None, "test_value", None)
+        text = "test_value"
+        self.assertEqual(node.to_html(), text)
+        # if tag is None, props should be ignored
+        node = LeafNode(None, "test_value", {"color": "red"})
+        text = "test_value"
+        self.assertEqual(node.to_html(), text)
+        node = LeafNode("test_tag", "test_value", {"color": "red"})
+        text = "<test_tag color=red>test_value</test_tag>"
 
 
 if __name__ == "__main__":
