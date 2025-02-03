99b11d787ea0af19c415b8c607c98e63cc8e1cd7
JulianPL <j.pape-lange@gmx.de>
2025-02-03 21:50:34 +0100

    :sparkles: Add recursive copy

--- src/main.py
+++ src/main.py
@@ -1,8 +1,35 @@
-from textnode import TextType
-from textnode import TextNode
+import shutil
+import os
+
+PUBLIC_FOLDER = "./public"
+STATIC_FOLDER = "./static"
 
 def main():
-    print(TextNode("This is a text node", TextType.BOLD, "https://boot.dev"))
+    shutil.rmtree(PUBLIC_FOLDER, True)
+    files, folders = list_dir_recursively(STATIC_FOLDER)
+    for folder in folders:
+        new_folder = PUBLIC_FOLDER+folder[len(STATIC_FOLDER):]
+        os.mkdir(new_folder)
+    for file in files:
+        new_file = PUBLIC_FOLDER+file[len(STATIC_FOLDER):]
+        if os.path.exists(file):
+            shutil.copy(file, new_file)
+    
+    
+
+def list_dir_recursively(path):
+    files_and_dirs_raw = os.listdir(path)
+    files = []
+    folders = [path]
+    for file_or_dir_raw in files_and_dirs_raw:
+        file_or_dir = os.path.join(path, file_or_dir_raw)
+        if os.path.isfile(file_or_dir):
+            files.append(file_or_dir)
+        else:
+            files_new, folders_new = list_dir_recursively(file_or_dir)
+            files += files_new
+            folders += folders_new
+    return files, folders
 
 if __name__ == "__main__":
     main()
