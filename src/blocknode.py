aae27cee9515cb8852199e23267ae0d14d1ed478
JulianPL <j.pape-lange@gmx.de>
2025-02-06 23:36:24 +0100

    :sparkles: Add BlockNodes

--- src/blocknode.py
+++ src/blocknode.py
@@ -1,15 +1,126 @@
+import logging
+
 from enum import Enum
 import re
 from htmlnode import ParentNode
 from textnode import text_to_textnodes, text_node_to_html_node
 
+logger = logging.getLogger(__name__)
+
 class BlockType(Enum):
+    MAIN = "main"
     PARAGRAPH = "paragraph"
     HEADING = "heading"
     CODE = "code"
     QUOTE = "quote"
     UNORDERED_LIST = "unordered_list"
     ORDERED_LIST = "ordered_list"
+
+class BlockNode:
+    def __init__(self, content, block_type):
+        self.content = content
+        self.block_type = block_type
+    
+    def __eq__(self, other):
+        return self.content == other.content and self.block_type == other.block_type
+
+    def __repr__(self):
+        return f"BlockNode({self.content}, {self.block_type.value})"
+
+def extract_title(markdown):
+    # Search for line with "# {Title}" preceded and succeeded by only whitespace
+    titles = re.findall(r"(?:^|\n\s*)# ([^\n]*)(?:$|\n)", markdown)
+    if not titles:
+        logger.warning(f"No title found")
+        return ""
+    return titles[0].strip()
+
+def markdown_to_block_nodes(markdown):
+    if markdown == "":
+        return []
+    
+    head, tail, _ = split_in_two(markdown, '\n')
+    
+    # Ignore empty heads
+    if head.strip() == "":
+        return markdown_to_block_nodes(tail)
+    
+    # Headings start with 1-6 times '#' and are followed by a space and at least one none-whitespace
+    # Headings are single lines
+    if re.fullmatch(r"\s*#{1,6} .*\S.*", head):
+        return [BlockNode(head.strip(), BlockType.HEADING)] + markdown_to_block_nodes(tail)
+    
+    # Code blocks both start as well as end with "```"
+    # Code blocks end at the second "```" or at the end of the markdown block (with a logger warning)
+    # Line with the closing "```" is defined to end with "```"
+    if re.match(r"\s*```", head):
+        space, code, tail_raw, count = split_in_three(markdown, "```")
+        if count <= 2:
+            logger.warning(f"Unclosed code block starting at: {head}")
+        _, tail, _ = split_in_two(tail_raw, '\n')
+        return [BlockNode(code.rstrip(), BlockType.CODE)] + markdown_to_block_nodes(tail)
+    
+    # Quotes start with "> " at each line, independent of indentation
+    # Quotes end with first line without "> " or at the end of the file
+    if re.match(r"\s*> ", head):
+        content = ""
+        while re.match(r"\s*> ", head):
+            content += head.split('> ', 1)[1] + "\n"
+            head, tail, _ = split_in_two(tail, '\n')
+        return [BlockNode(content.rstrip(), BlockType.QUOTE)] + markdown_to_block_nodes(f"{head}\n{tail}")
+    
+    # Unordered lists start with either "s* " or "s- " in each line for some single line whitestring
+    # Unordered lists end with first line without "s* " or "s- " or "s " or at the end of the file
+    if re.match(r"\s*(\*|-) ", head):
+        space = re.match(r"(\s*)(\*|-) ", head)[1]
+        content = ""
+        while re.match(rf"{space}(\*|-|) ", head):
+            content += head + "\n"
+            head, tail, _ = split_in_two(tail, '\n')
+        return [BlockNode(content.rstrip(), BlockType.UNORDERED_LIST)] + markdown_to_block_nodes(f"{head}\n{tail}")
+    
+    # Ordered lists start with either "s\d+. " in each line for some single line whitestring
+    # Ordered lists end with first line without "s\d+. " or "s " or at the end of the file
+    if re.match(r"\s*\d+. ", head):
+        space = re.match(r"(\s*)\d+. ", head)[1]
+        content = ""
+        while re.match(rf"{space}(\d+.|) ", head):
+            content += head + "\n"
+            head, tail, _ = split_in_two(tail, '\n')
+        return [BlockNode(content.rstrip(), BlockType.ORDERED_LIST)] + markdown_to_block_nodes(f"{head}\n{tail}")
+    
+    # Paragraphs are the default case
+    # A paragraph ends with an empty line
+    markdown_split = re.split(r"\n\s*\n", markdown, 1)
+    head = markdown_split[0]
+    tail = "" if len(markdown_split) == 1 else markdown_split[1]
+    return [BlockNode(head.strip(), BlockType.PARAGRAPH)] + markdown_to_block_nodes(tail)
+
+def split_in_two(text, separator):
+    text_split = text.split(separator, 1)
+    head = text_split[0]
+    tail = "" if len(text_split) == 1 else text_split[1]
+    return head, tail, len(text_split)
+
+def split_in_three(text, separator):
+    text_split = text.split(separator, 2)
+    head = text_split[0]
+    middle = "" if len(text_split) <= 1 else text_split[1]
+    tail = "" if len(text_split) <= 2 else text_split[2]
+    return head, middle, tail, len(text_split)
+
+
+
+
+
+
+
+
+def markdown_to_html(markdown):
+    blocks_text = markdown_to_blocks(markdown)
+    blocks_with_types = [(block_text, block_to_block_type(block_text)) for block_text in blocks_text]
+    children = [block_to_html_node(block_with_type) for block_with_type in blocks_with_types]
+    return ParentNode("div", children)
 
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
@@ -32,7 +143,7 @@ def block_to_block_type(block):
     if all(line.startswith(">") for line in lines):
         return BlockType.QUOTE
     
-    # Unordered lists start with either "*" or "-" in each line
+    # Unordered lists start with either "* " or "- " in each line
     if all(line.startswith("* ") or line.startswith("- ") for line in lines):
         return BlockType.UNORDERED_LIST
     
@@ -73,16 +184,5 @@ def block_to_html_node(block_with_type):
 def text_to_children(text):
     return [text_node_to_html_node(text_node) for text_node in text_to_textnodes(text)]
 
-def markdown_to_html(markdown):
-    blocks_text = markdown_to_blocks(markdown)
-    blocks_with_types = [(block_text, block_to_block_type(block_text)) for block_text in blocks_text]
-    children = [block_to_html_node(block_with_type) for block_with_type in blocks_with_types]
-    return ParentNode("div", children)
 
-def extract_title(markdown):
-    titles = re.findall(r"(?:^|\n)# ([^\n]*)(?:$|\n)", markdown)
-    if not titles:
-        raise Exception("No title found")
-    return titles[0].strip()
-    
-    
+
--- src/test_blocknode.py
+++ src/test_blocknode.py
@@ -1,6 +1,75 @@
 import unittest
 
-from blocknode import BlockType, markdown_to_blocks, block_to_block_type, markdown_to_html, extract_title
+from blocknode import BlockType, BlockNode, markdown_to_block_nodes, markdown_to_blocks, block_to_block_type, markdown_to_html, extract_title
+
+class TestTitleExtraction(unittest.TestCase):
+    def test_extract_title(self):
+        with self.assertNoLogs('blocknode', level='WARNING') as logs:
+            actual = extract_title("# Hello")
+        expected = "Hello"
+        self.assertEqual(actual, expected)
+        with self.assertNoLogs('blocknode', level='WARNING') as logs:
+            actual = extract_title("test\n# Hello\ntest")
+        expected = "Hello"
+        self.assertEqual(actual, expected)
+        with self.assertNoLogs('blocknode', level='WARNING') as logs:
+            actual = extract_title("test\n  # Hello\ntest")
+        expected = "Hello"
+        self.assertEqual(actual, expected)
+        with self.assertLogs('blocknode', level='WARNING') as logs:
+            actual = extract_title("test\nHello\ntest")
+        expected = ""
+        self.assertEqual(actual, expected)
+        self.assertEqual(len(logs.output), 1)
+
+class TestMarkdownToBlockNodeConversion(unittest.TestCase):
+    def test_markdown_to_block_nodes(self):
+        actual = markdown_to_block_nodes("")
+        expected = []
+        self.assertEqual(actual, expected)
+        actual = markdown_to_block_nodes("   \n ")
+        expected = []
+        self.assertEqual(actual, expected)
+        actual = markdown_to_block_nodes("# Heading")
+        expected = [BlockNode("# Heading", BlockType.HEADING)]
+        self.assertEqual(actual, expected)
+        actual = markdown_to_block_nodes("# Heading \n  ## Subheading")
+        expected = [BlockNode("# Heading", BlockType.HEADING), BlockNode("## Subheading", BlockType.HEADING)]
+        self.assertEqual(actual, expected)
+        actual = markdown_to_block_nodes("```language\ncode```")
+        expected = [BlockNode("language\ncode", BlockType.CODE)]
+        self.assertEqual(actual, expected)
+        actual = markdown_to_block_nodes("```\ncode```")
+        expected = [BlockNode("\ncode", BlockType.CODE)]
+        self.assertEqual(actual, expected)
+        actual = markdown_to_block_nodes("> Quoteline 1\n> Quoteline 2\n> Quoteline 3\n# Heading")
+        expected = [BlockNode("Quoteline 1\nQuoteline 2\nQuoteline 3", BlockType.QUOTE), BlockNode("# Heading", BlockType.HEADING)]
+        self.assertEqual(actual, expected)
+        actual = markdown_to_block_nodes("- This\n- is\n- unordered 3\n# Heading")
+        expected = [BlockNode("- This\n- is\n- unordered 3", BlockType.UNORDERED_LIST), BlockNode("# Heading", BlockType.HEADING)]
+        self.assertEqual(actual, expected)
+        actual = markdown_to_block_nodes("- This\n- is\n unordered 3\n# Heading")
+        expected = [BlockNode("- This\n- is\n unordered 3", BlockType.UNORDERED_LIST), BlockNode("# Heading", BlockType.HEADING)]
+        self.assertEqual(actual, expected)
+        actual = markdown_to_block_nodes("- This\n* is\n unordered 3\n# Heading")
+        expected = [BlockNode("- This\n* is\n unordered 3", BlockType.UNORDERED_LIST), BlockNode("# Heading", BlockType.HEADING)]
+        self.assertEqual(actual, expected)
+        actual = markdown_to_block_nodes("* This\n* is\n unordered 3\n# Heading")
+        expected = [BlockNode("* This\n* is\n unordered 3", BlockType.UNORDERED_LIST), BlockNode("# Heading", BlockType.HEADING)]
+        self.assertEqual(actual, expected)
+        actual = markdown_to_block_nodes("42. This\n12. is\n ordered 3\n# Heading")
+        expected = [BlockNode("42. This\n12. is\n ordered 3", BlockType.ORDERED_LIST), BlockNode("# Heading", BlockType.HEADING)]
+        self.assertEqual(actual, expected)
+        actual = markdown_to_block_nodes("This is a paragraph")
+        expected = [BlockNode("This is a paragraph", BlockType.PARAGRAPH)]
+        self.assertEqual(actual, expected)
+        actual = markdown_to_block_nodes("This is a paragraph\n# with a second line\n")
+        expected = [BlockNode("This is a paragraph\n# with a second line", BlockType.PARAGRAPH)]
+        self.assertEqual(actual, expected)
+        actual = markdown_to_block_nodes("This is a paragraph\n   \n# with a second line\n")
+        expected = [BlockNode("This is a paragraph", BlockType.PARAGRAPH), BlockNode("# with a second line", BlockType.HEADING)]
+        self.assertEqual(actual, expected)
+        
 
 class TestBlockSplit(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -53,16 +122,7 @@ class TestBlockConversion(unittest.TestCase):
         expected = "ParentNode(div, [ParentNode(ol, [ParentNode(li, [LeafNode(None, 1, None)], None), ParentNode(li, [LeafNode(None, 2, None)], None)], None)], None)"
         self.assertEqual(actual, expected)
 
-class TestHelpers(unittest.TestCase):
-    def test_extract_title(self):
-        actual = extract_title("# Hello")
-        expected = "Hello"
-        self.assertEqual(actual, expected)
-        actual = extract_title("test\n# Hello\ntest")
-        expected = "Hello"
-        self.assertEqual(actual, expected)
-        with self.assertRaises(Exception):
-            extract_title("test\n## Hello\ntest")
+
 
 if __name__ == "__main__":
     unittest.main()
